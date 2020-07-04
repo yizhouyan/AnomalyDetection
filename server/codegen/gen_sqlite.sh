a7bcfb9b76fd47cdeed87a66de9c2c1612239258
yizhouyan <yizhouyan9132@outlook.com>
2020-07-04 09:24:04 -0400

    change maven link

--- server/codegen/gen_sqlite.sh
+++ server/codegen/gen_sqlite.sh
@@ -2,10 +2,10 @@
 rm -rf jars/
 rm sqlite/*jar
 mkdir -p jars
-wget http://central.maven.org/maven2/org/jooq/jooq/3.8.4/jooq-3.8.4.jar -O jars/jooq-3.8.4.jar
-wget http://central.maven.org/maven2/org/jooq/jooq-meta/3.8.4/jooq-meta-3.8.4.jar -O jars/jooq-meta-3.8.4.jar
-wget http://central.maven.org/maven2/org/jooq/jooq-codegen/3.8.4/jooq-codegen-3.8.4.jar -O jars/jooq-codegen-3.8.4.jar
-wget http://central.maven.org/maven2/org/xerial/sqlite-jdbc/3.15.1/sqlite-jdbc-3.15.1.jar -O sqlite/sqlite-jdbc-3.15.1.jar
+wget https://repo.maven.apache.org/maven2/org/jooq/jooq/3.8.4/jooq-3.8.4.jar -O jars/jooq-3.8.4.jar
+wget https://repo.maven.apache.org/maven2/org/jooq/jooq-meta/3.8.4/jooq-meta-3.8.4.jar -O jars/jooq-meta-3.8.4.jar
+wget https://repo.maven.apache.org/maven2/org/jooq/jooq-codegen/3.8.4/jooq-codegen-3.8.4.jar -O jars/jooq-codegen-3.8.4.jar
+wget https://repo.maven.apache.org/maven2/org/xerial/sqlite-jdbc/3.15.1/sqlite-jdbc-3.15.1.jar -O sqlite/sqlite-jdbc-3.15.1.jar
 
 cat ./sqlite/createDb.sql | sqlite3 anomalydetection.db  &&
 cat ./sqlite/createDb.sql | sqlite3 anomalydetection_test.db  &&
